ce2398066886fc155c287702db80c36cf5af0e7a
JustAStudent-py <krampus015@gmail.com>
2026-03-23 14:00:09 -0400

    Introduce the exercise of the day29

---
+++ day29/ex01.py
@@ -0,0 +1,25 @@
+def verify_numbers(numbers):
+    count_dict = {}
+
+    # Count occurrences (1 loop só)
+    for n in numbers:
+        if n in count_dict:
+            count_dict[n] += 1
+        else:
+            count_dict[n] = 1
+
+    duplicates = []
+    unique_numbers = []
+
+    # Classify numbers
+    for n, count in count_dict.items():
+        if count == 1:
+            unique_numbers.append(n)
+        else:
+            duplicates.append(n)
+
+    return duplicates, unique_numbers, count_dict
+
+
+numbers = [1, 2, 2, 3, 4, 4, 4, 5]
+print(verify_numbers(numbers))
